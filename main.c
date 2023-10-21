760c06a41228624912872353815edb2f1a8d7aeb
tvvmvn <tvvmvn@gmail.com>
2023-10-21 14:42:41 +0900

    10/21

--- main.c
+++ main.c
@@ -4,103 +4,103 @@
 #include <stdlib.h>
 #include <time.h>
 
-char str[] = "123456789";
-int win = 0;
-int count = 0;
+char board[] = "123456789";
 
 int bingo() {
-  if (str[3] == str[4] && str[4] && str[5]) {
+  if (board[3] == board[4] && board[4] == board[5]) {
     return 1;  
-  } 
-  if (str[1] == str[4] && str[4] == str[7]) {
+  } else if (board[1] == board[4] && board[4] == board[7]) {
     return 1;
-  }
-  if (str[2] == str[4] && str[4] == str[6]) {
+  } else if (board[2] == board[4] && board[4] == board[6]) {
     return 1;
-  }
-  if (str[0] == str[4] && str[4] == str[8]) {
+  } else if (board[0] == board[4] && board[4] == board[8]) {
     return 1;
-  }
-  if (str[0] == str[3] && str[3] == str[6]) {
+  } else if (board[0] == board[3] && board[3] == board[6]) {
     return 1;
-  }
-  if (str[0] == str[1] && str[1] == str[2]) {
+  } else if (board[0] == board[1] && board[1] == board[2]) {
     return 1;
-  }
-  if (str[2] == str[5] && str[5] == str[8]) {
+  } else if (board[2] == board[5] && board[5] == board[8]) {
     return 1;
-  }
-  if (str[6] == str[7] && str[7] == str[8]) {
+  } else if (board[6] == board[7] && board[7] == board[8]) {
     return 1;
+  } else {
+    return 0;
   }
-  return 0;
+}
+
+void drawboard() {
+  printf("---\n");
+  printf("%c %c %c\n", board[0], board[1], board[2]);
+  printf("%c %c %c\n", board[3], board[4], board[5]);
+  printf("%c %c %c\n", board[6], board[7], board[8]);
+  printf("---\n");
 }
 
 int main() {
   int n;
   int _n;
   int com;
+  int win;
+  int count = 0;
 
-  srand(time(NULL));   
+  srand(time(NULL));
 
   while (1) {
-    // Board
-    printf("* TIC TAC TOE *\n");
-    printf("%c %c %c\n", str[0], str[1], str[2]);
-    printf("%c %c %c\n", str[3], str[4], str[5]);
-    printf("%c %c %c\n", str[6], str[7], str[8]);
-    printf("-\n");
+    drawboard();
 
-    if (win == 1) {
-      printf("YOU WIN!\n");
-      break;
-    }
-
-    if (win == 2) {
-      printf("YOU LOSE\n");
-      break;
-    }
-
-    if (count > 8) {
-      printf("DRAWN\n");
-      break;
-    }
-
-    // User
+    // USER
     while(1) {
       printf("choose a number between 1 - 9\n");
       scanf("%d", &n);
 
       _n = n - 1;
 
-      if (str[_n] != 'O' && str[_n] != 'X') {
-        str[_n] = 'O';
+      if (board[_n] != 'O' && board[_n] != 'X') {
+        board[_n] = 'O';
+        count++;
         break;
       } else {
-        printf("occupied\n");
+        drawboard();
+        printf("occupied. try again\n");
       }
     }
 
     if (bingo()) {
       win = 1;
+      break;
     }
 
-    count++;
+    if (count > 8) {
+      win = 0;
+      break;
+    }
 
     // COM
-    while(bingo() != 1) {
-      com = rand() % 9;  
+    while(1) {
+      com = rand() % 9;
 
-      if (str[com] != 'O' && str[com] != 'X') {
-        str[com] = 'X';
+      if (board[com] != 'O' && board[com] != 'X') {
+        board[com] = 'X';
+        count++;
         break;
       } 
     }
 
-    if (win == 0 && bingo()) {
+    if (bingo()) {
       win = 2;
+      break;
     }
+  }
 
-    count++;
+  drawboard();
+
+  if (win == 1) {
+    printf("YOU WIN\n");
+  } 
+  if (win == 2) {
+    printf("YOU LOSE\n");
+  } 
+  if (win == 0) {
+    printf("DRAWN\n");
   }
 }
