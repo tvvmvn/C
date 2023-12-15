59c41ee65f5948a89773668a4e96b2a3cf28f24f
tvvmvn <tvvmvn@gmail.com>
2023-12-15 09:50:12 +0900

    12/15

--- chosung_game/main.c
+++ chosung_game/main.c
@@ -19,13 +19,10 @@ int rd_index() {
 }
 
 int main() {
-  char store[] = "ㄱㄴㄷㄹㅁㅂㅅㅇㅈㅊㅋㅌㅍㅎ"; // 14 characters
+  // 14 Korean alphabets (3 byte)
+  char store[] = "ㄱㄴㄷㄹㅁㅂㅅㅇㅈㅊㅋㅌㅍㅎ"; 
   int i1;
   int i2;
-  int i3;
-  int i4;
-  int i5;
-  int i6;
   char c;
   int i = 1;
 
@@ -35,16 +32,12 @@ int main() {
 
   while (i < 10) {
     i1 = rd_index();
-    i2 = i1 + 1;
-    i3 = i1 + 2;
-    i4 = rd_index();
-    i5 = i4 + 1;
-    i6 = i4 + 2;
+    i2 = rd_index();
 
     printf("%d. ", i);
-    printf("%c%c%c", store[i1], store[i2], store[i3]);
-    printf("%c%c%c\n", store[i4], store[i5], store[i6]);
-    printf("press enter to pass >>");
+    printf("%c%c%c", store[i1], store[i1 + 1], store[i1 + 2]);
+    printf("%c%c%c\n", store[i2], store[i2 + 1], store[i2 + 2]);
+    printf("press enter to next >>");
 
     c = getchar();
 
