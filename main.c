c13a2569fc17c518608a5351effbda1c9eca6a6f
tvvmvn <tvvmvn@gmail.com>
2023-12-27 11:06:03 +0900

    12/27

--- main.c
+++ main.c
@@ -4,7 +4,6 @@
 #include <stdlib.h>
 #include <time.h>
 
-
 struct Piece {
   int id;
   char name;
@@ -13,16 +12,35 @@ struct Piece {
   int selected; // 0 or 1
 };
 
+void printBoard(struct Piece pieces[4], int board[4][4]) {
+  for (int r=0; r<4; r++) {
+    for (int c=0; c<4; c++) {
+      // thare is a piece
+      if (board[r][c] != 0) {
+        for (int i=0; i<4; i++) {
+          if (pieces[i].id == board[r][c]) {
+            printf("%c ", pieces[i].name);
+          }
+        }
+      // empty cell
+      } else {
+        printf("# ");
+      }
+    }
+    printf("\n");
+  }
+}
+
 int main() {
   int row;
   char col;
   int row_d;
   char col_d;
-  int turn = 2;
+  int turn = 1;
   int n;
   int m;
 
-  struct Piece pieces[] = {
+  struct Piece pieces[4] = {
     {1, 'a', 1, 1, 0},
     {2, 'b', 1, 1, 0},
     {3, 'c', 2, 1, 0},
@@ -38,28 +56,15 @@ int main() {
 
   // PLAY
   while (1) {
-
     // Print board
-    for (int r=0; r<4; r++) {
-      for (int c=0; c<4; c++) {
-        // thare is a piece
-        if (board[r][c] != 0) {
-          for (int i=0; i<4; i++) {
-            if (pieces[i].id == board[r][c]) {
-              printf("%c ", pieces[i].name);
-            }
-          }
-        // empty cell
-        } else {
-          printf("# ");
-        }
-      }
-      printf("\n");
-    }
+    printBoard(pieces, board);
 
+    turn = turn == 1 ? 2 : 1;
+    
     // Choose a piece
     while (1) {
       printf("choose a piece to move\n");
+
 
       scanf("%d %c", &row, &col);
 
@@ -75,7 +80,7 @@ int main() {
 
         for (int i=0; i<4; i++) {
           if (board[row][n] == pieces[i].id) {
-            if (pieces[i].team == 2) {
+            if (pieces[i].team == turn) {
               stop = 1;
             } else { // wrong team 
               printf("not your team. try again\n");
@@ -100,18 +105,25 @@ int main() {
       if (col_d == 'c') m = 2;
       if (col_d == 'd') m = 3;
 
+      int r = row == row_d && n + 1 == m;
+
       // validate dest
-      if (board[row_d][m] == 0) {
-        break;
+      if (r == 0) {
+        printf("Not takeable. try again\n");
       } else {
         int stop = 0;
+        int id = board[row_d][m];
 
-        for (int i=0; i<4; i++) {
-          if (board[row_d][m] == pieces[i].id) {
-            if (pieces[i].team == 2) {
-              printf("there is a piece already\n");
-            } else { // wrong team 
+        if (id == 0) {
+          break;
+        }
+
+        for (int i=0; i<10; i++) {
+          if (pieces[i].id == id) {
+            if (pieces[i].team != turn) { // take enemy
               stop = 1;
+            } else {
+              printf("already a piece. try again\n");
             }
           }
         }
@@ -122,30 +134,10 @@ int main() {
       }
     }
 
+    // move
     int temp = board[row][n];
 
     board[row][n] = 0;
     board[row_d][m] = temp;
-
-    // print board again
-    for (int r=0; r<4; r++) {
-      for (int c=0; c<4; c++) {
-        // there is a piece
-        if (board[r][c] != 0) {
-          for (int i=0; i<4; i++) {
-            if (pieces[i].id == board[r][c]) {
-              printf("%c ", pieces[i].name);
-            }
-          }
-        // empty celll
-        } else {
-          printf("# ");
-        }
-      }
-      printf("\n");
-    }
-
-    //GAME END
-    break;
   }
 }
