302c29c745996c158db66dc5ca8fb877ee3fb30d
tvvmvn <tvvmvn@gmail.com>
2024-01-17 17:03:36 +0900

    1/17

--- main.c
+++ main.c
@@ -14,41 +14,78 @@ enum Pieces {
 };
 
 struct Piece {
-  int name;
-  char crds[2];
+  enum Pieces name;
+  int crds[2];
   int legal[8][8];
 };
 
 /* contants */
+
 const char rows[] = "12345678";
 const char cols[] = "abcdefgh";
 
 /* variables */
 
+struct Piece pieces[3] = {
+  {PAWN, {6, 0}},
+  {KNIGHT, {7, 1}},
+  {ROOK, {7, 7}},
+};
+
 /* functions */
 
+void pawnmv(struct Piece* pawn) {
+  if (r == pawn->crds[0] - 1 && c == pawn->crds[1]) {
+    pawn->legal[r][c] = 1;
+  }
+}
+
+void knightmv(struct Piece* knight) {
+  if (r == knight->crds[0] - 2 && c == knight->crds[1] - 1) {
+    knight->legal[r][c] = 1;
+  }
+
+  if (r == knight->crds[0] - 2 && c == knight->crds[1] + 1) {
+    knight->legal[r][c] = 1;
+  }
+}
+
+// function to tell that check is takeable or not
+
+void rookmv(struct Piece* rook) {
+  // up
+  for (int r = rook->crds[0]; r >= 0; r--) {
+    if () { // there is a piece
+      break;
+    }
+    rook->legal[r][rook->crds[1]] = 1;
+  }
+}
+
+void bishopmv(struct Piece* bishop, int r, int c) {
+  if (bishop->crds[1] == c) {
+    bishop->legal[r][c] = 1;
+  }
+
+  if (bishop->crds[0] == r) {
+    bishop->legal[r][c] = 1;
+  }
+}
+
 int main() {
-
-  struct Piece pieces[1] = {
-    {PAWN, {6, 0}}
-  };
-
-  for (int i = 0; i < 1; i++) {
-    if (pieces[i].name == PAWN) {      
-
-      for (int r = 0; r < 8; r++) {
-        for (int c = 0; c < 8; c++) { // (5, 0) is takeable.
-          if (r == pieces[i].crds[0] - 1 && c == pieces[i].crds[1]) {
-            pieces[i].legal[r][c] = 1;
-          }
-        }
-      }
+  for (int i = 0; i < 3; i++) {  
+    if (pieces[i].name == PAWN) {
+      pawnmv(&pieces[i]);
+    } else if (pieces[i].name == KNIGHT) {
+      knightmv(&pieces[i]);
+    } else if (pieces[i].name == ROOK) {
+      rookmv(&pieces[i]);
     }
   }
 
   for (int r = 0; r < 8; r++) {
     for (int c = 0; c < 8; c++) {
-      printf("%d ", pieces[0].legal[r][c]);
+      printf("%d ", pieces[2].legal[r][c]);
     }
     printf("\n");
   }
