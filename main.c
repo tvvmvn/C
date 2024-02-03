6d3ef04b271056a08b3a7847f57b4bca99ea1a97
tvvmvn <tvvmvn@gmail.com>
2024-02-03 22:48:14 +0900

    2/3

--- main.c
+++ main.c
@@ -6,7 +6,43 @@
 
 
 int main() {
-  int arr[] = {10, 20, 30};
 
-  printf("%d\n", *arr);
+  // CHECKMATE
+
+  // 1. king has place to avoid ?
+
+  for (int r = 0; r < 8; r++) {
+    for (int c = 0; c < 8; c++) {
+      if (king.legal[r][c] == 1) {
+        for (int i = 0; i < 32; i++) {
+          if (pieces[i].legal[r][c] == 1) {
+            king.legal[r][c] = 0;
+          }
+        }
+      }
+    }
+  }
+
+  // .. if king has legal, not checkmate!
+
+
+  // 2. can't remove threat?
+
+  for (int r = 0; r < 8; r++) {
+    for (int c = 0; c < 8; c++) {
+      if (pieces[i].legal[r][c] == 1) {
+        if (king.crds[0] == r && king.crds[1] == c) {
+          threat = pieces[i];
+        }
+      }
+    }
+  }
+
+  if (pieces[i].legal[r][c] == 1) {
+    if (threat.crds[0] == r && threat.crds[1] == c) {
+      // removing threat's done!
+      pieces[i].crds[0] = r;
+      pieces[i].crds[1] = c;
+    }
+  }
 }
