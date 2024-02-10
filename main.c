6a692b448eaa8979c043615bade24fa3946ffcbb
tvvmvn <tvvmvn@gmail.com>
2024-02-10 21:50:10 +0900

    2/10

--- main.c
+++ main.c
@@ -4,23 +4,15 @@
 #include <stdlib.h>
 #include <time.h>
 
+/*
+  state of point
+
+  0 not legal
+  1 (takeable) empty or opponent
+  2 same team
+*/
 
 int main() {}
-
-// # whether check is danger zone or not
-void isdangerzone(int r, int c) {
-  int n = 0;
-
-  for (int i = 0; i < 32; i++) {
-    if (pieces[i].team == BLACK) {
-      if (pieces[i].points[r][c] == 1) {
-        n = 1;
-      }
-    }
-  }
-
-  return n;
-}
 
 // # Get points of king
 void getpoints() {
@@ -28,24 +20,24 @@ void getpoints() {
   int r = king.crds[0] - 1;
   int c = king.crds[1] - 1;
 
-  int id = board[r][c];
+  int dangerzone = isdangerzone(r, c);
 
-  // empty
-  if (!id) {
-    int dangerzone = isdangerzone(r, c);
-
-    if (dangerzone) {
-      king.points[r][c] = 0;
-    } else {
-      king.points[r][c] = 1;
-    }
+  if (dangerzone) {
+    king.points[r][c] = 0;
   } else {
-    struct Piece piece = getpiecebyid(id);
-
-    if (piece.team == WHITE) {
-      king.points[r][c] = 0;
-    } else {
+    int id = board[r][c];
+  
+    // empty
+    if (!id) {
       king.points[r][c] = 1;
+    } else {
+      struct Piece piece = getpiecebyid(id);
+
+      if (piece.team == WHITE) {
+        king.points[r][c] = 0;
+      } else {
+        king.points[r][c] = 1;
+      }
     }
   }
 }
@@ -56,4 +48,22 @@ void isckmate() {
   if (isdangerzone(king.crds[0], king.crds[1])) {
     // and no place to avoid! -> all 0
   }
+}
+
+// # whether check is danger zone or not
+void isdangerzone(int r, int c) {
+  int n = 0;
+
+  for (int i = 0; i < 32; i++) {
+    if (pieces[i].team == BLACK) {
+      if (
+        pieces[i].points[r][c] == 1 
+        || pieces[i].points[r][c] == 2
+      ) {
+        n = 1;
+      }
+    }
+  }
+
+  return n;
 }
