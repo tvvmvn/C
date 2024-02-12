7a820e39c0cb9b39052580898b828682073565f1
tvvmvn <tvvmvn@gmail.com>
2024-02-12 21:13:11 +0900

    2/12

--- main.c
+++ main.c
@@ -12,6 +12,34 @@
   2 same team (potentially takeable by opponent)
 */
 
+/*
+  checkmate check process
+
+  1 king under threats now, then
+
+  2 freeze all
+
+  3 
+  3-1. only 1 threat
+    3-1-1. saver exists
+      3-1-1-1. king has [1]
+      unfreeze him.
+      3-1-1-2. king has no [1]
+      unfreeze him.
+    
+    3-1-2. saver doesn't exists
+      3-1-2-1. king has [1]
+      keep playing
+      3-1-2-2. king has no [1]
+      checkmate!
+
+  3-2. more than 2 threats 
+    3-2-1. king has [1]
+    keep playing
+    3-2-2. king has no [1] 
+    checkmate!
+*/
+
 int main() {}
 
 // # Get points of king
@@ -42,38 +70,8 @@ void getpoints() {
   }
 }
 
-// # checkmate
-void ischeckmate() {
-  // 1 king in threat now, then
-  if (isdangerzone(king.crds[0], king.crds[1])) {
-    /*
-    2 freeze all
-
-    3 
-    3-1. only 1 threat
-      3-1-1. saver exists
-        3-1-1-1. king has [1]
-        unfreeze him.
-        3-1-1-2. king has no [1]
-        unfreeze him.
-      
-      3-1-2. saver doesn't exists
-        3-1-2-1. king has [1]
-        keep playing
-        3-1-2-2. king has no [1]
-        checkmate!
-
-    3-2. more than 2 threats 
-      3-2-1. king has [1]
-      keep playing
-      3-2-2. king has no [1] 
-      checkmate!
-    */
-  }
-}
-
-// # whether check is danger zone or not
-void isdangerzone(int r, int c) {
+// # whether check target is danger zone or not
+int isdangerzone(int r, int c) {
   int n = 0;
 
   for (int i = 0; i < 32; i++) {
@@ -90,3 +88,48 @@ void isdangerzone(int r, int c) {
   return n;
 }
 
+// # Is king under threats?
+int isthreatened(int r, int c) {
+  int n = 0;
+
+  for (int i = 0; i < 32; i++) {
+    if (pieces[i].team == BLACK) {
+      if (pieces[i].points[r][c] == 1) {
+        n = 1;
+      }
+    }
+  }
+
+  return n;
+}
+
+// # get threat count
+int get_threat_cnt(int r, int c) {
+  int count = 0;
+
+  for (int i = 0; i < 32; i++) {
+    if (pieces[i].team == BLACK) {
+      if (pieces[i].points[r][c] == 1) {
+        count++;
+      }
+    }
+  }
+
+  return count;
+}
+
+// # checkmate
+void ischeckmante() {
+  if (isthreatened(king.crds[0], king.crds[1])) {
+    
+    // freeze all..
+
+    int threatcnt = get_threat_cnt(king.crds[0], king.crds[1]);
+
+    if (threatcnt == 1) {
+    
+    } else { // more than 2 threats
+    
+    }
+  }
+}
