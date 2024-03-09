a94a2d48014306fa5ddc4ef65578958de8dbe286
tvvmvn <tvvmvn@gmail.com>
2024-03-09 17:02:08 +0900

    3/9

--- main.c
+++ main.c
@@ -2,11 +2,7 @@
 #include <string.h>
 #include <math.h>
 #include <stdlib.h>
-#include <time.h>
 
 int main() {
-  printf("%d\n", 1 + 2); // 3
-  printf("%d\n", 1 - 2); // -1
-  printf("%d\n", 1 * 2); // 2
-  printf("%d\n", 2 / 1); // 2
+  printf("hello tutorials!");
 }
--- tutorials/README.md
+++
@@ -1,7 +0,0 @@
-# C Tutorials
-
-1. basic
-2. format specifier
-3. data type
-4. string
-5. operator
